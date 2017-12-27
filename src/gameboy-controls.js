9203aad021c2b4786f0d81b9a6443a92bc68adaa
JimTheMan <mrdotjim@gmail.com>
2017-12-27 11:08:58 -0500

    initial commit

---
+++ src/gameboy-controls.js
@@ -0,0 +1,18 @@
+
+class GameboyControls {
+
+
+    constructor() {
+        console.log('I\'m a goober')
+
+    }
+
+
+    derp () {
+        var xFoo = document.createElement('x-foo');
+        xFoo.addEventListener('click', function(e) {
+            alert('Thanks!');
+        });
+    }
+
+}
